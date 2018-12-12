e489cdbb6243a2766ce61a24c201b234d524d844
kensun619 <kensun619@gmail.com>
2018-12-12 15:56:12 -0600

    finish add_to_usage

--- balancer/profileCalculator.py
+++ balancer/profileCalculator.py
@@ -3,6 +3,7 @@ import const
 from collections import defaultdict
 import numpy as np
 import time
+from workerTracker import running_tasks
 
 def calculate(dataFile):
     insert_profile("test", 0, 100)
@@ -32,18 +33,18 @@ def getUsage():
 def cal_total_usage(active_profiles, current_timestamp, granularity):
     max_task_length = 20 # maximum task length, unit second
     total_usage = np.array([0]* max_task_length/granularity)
-
+    active_profiles = running_tasks()
     for each_profile_obj in active_profiles:
         add_to_usage(total_usage, each_profile_obj, current_timestamp)
 
-    return usage, interval
+    return total_usage
 
 def add_to_usage(total_usage, each_profile_obj, current_timestamp, granularity):
     # each profile here starts earlier than current timestamp and ends later than current timestamp
-    profile_start = each_profile_obj[0]
-    profile_end = each_profile_obj[1]
-    profile_usage = each_profile_obj[2]
-    profile_interval = each_profile_obj[3]
+    profile_start = each_profile_obj["start"]
+    profile_end = each_profile_obj["end"]
+    profile_usage = each_profile_obj["usage"]
+    profile_interval = each_profile_obj["interval"]
     tmp_timestamp = profile_start
     aligned_interval = None
     aligned_usage = None
@@ -57,11 +58,18 @@ def add_to_usage(total_usage, each_profile_obj, current_timestamp, granularity):
             aligned_interval.append(profile_interval[i+1: -1])
             aligned_usage.append(profile_usage[i+1: -1])
     # convert aligned_usage and aligned_interval to the form needed for sceduler
-    total_interval_len = sum(aligned_interval)
-    total_ticks = np.int(np.ceil(total_interval_len/ granularity))
+    cum_sum_profile_interval = np.cumsum(aligned_interval)
+    ticks = np.int(np.ceil(cum_sum_profile_interval/ granularity))
+    for i in range(ticks):
+        if i == 0:
+            start = 0
+        else:
+            start = ticks[i-1]+1
+        end = ticks[i]
+        total_usage[start:end] = total_usage[start:end] + aligned_usage[i]
 
-
-
+    if(np.any(total_usage > 1.0)):
+        print("Error")
 
 def addUsage(worker, task):
     taskProfile = getTaskProfiles()[task]
